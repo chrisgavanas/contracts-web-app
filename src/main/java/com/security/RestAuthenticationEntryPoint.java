fe04c68b4429e2f8e1a778e1a319c0afb9a4ea1b
Christos Peristeris <christos.peristeris@camelotglobal.com>
2019-01-16 19:15:23 +0200

    security - Adds security related classes.

---
+++ src/main/java/com/security/RestAuthenticationEntryPoint.java
@@ -0,0 +1,20 @@
+package com.security;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(final HttpServletRequest request, final HttpServletResponse response, final AuthenticationException authException) throws IOException, ServletException {
+        //Authentication failed, send error response.
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+
+}
